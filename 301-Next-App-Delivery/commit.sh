d2e24b69c960b6d3f6ec85ce551fda1982e1590b
Matt Mabis <m.mabis@f5.com>
2024-12-11 11:31:51 -0700

    remove 301 code - moved to branch

--- 301-Next-App-Delivery/commit.sh
+++
@@ -1,6 +0,0 @@
-git config --global user.email "m.mabis@f5.com"
-git config --global user.name "vdi-tech-guy"
-git config --global push.default simple
-git add -A
-git commit -m "Code Updates - $(date)"
-git push 
